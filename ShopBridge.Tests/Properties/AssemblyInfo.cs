f0f1e18576d00921f4d8d4b01c31c1c3094de5e0
Vrushh47 <47249318+Vrushh47@users.noreply.github.com>
2020-08-26 09:51:01 +0530

    Files added

---
+++ ShopBridge.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShopBridge.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShopBridge.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0031f1dc-2053-4e3d-98b0-dfff6da392f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ShopBridge.Tests/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopBridge.Controllers;
+using System.Web.Mvc;
+using DAL;
+using BLL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopBridge.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int TestID=0;
+        [TestMethod]
+        public void AllItemDisplay()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult view = controller.AllItemDisplay() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(view);
+        }
+        [TestMethod]
+        public void Details()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult view = controller.Details(0) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(view);
+        }
+        [TestMethod]
+        public void Add()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult view = controller.Add() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(view);
+        }
+        [TestMethod]
+        public void AddItem()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            ItemModel itemModel = new ItemModel();
+            itemModel.Name = "Test";
+            //Act
+            ViewResult view = controller.Add(itemModel) as ViewResult;
+
+            TestID = view.ViewBag.Id;
+            //Assert
+            Assert.AreNotEqual(0,TestID);
+        }
+        [TestMethod]
+        public void EditItem()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            ItemModel itemModel = new ItemModel();
+            using (var context = new ShopBridgeDBEntities())
+            {
+                TestID = context.ItemMasters.Where(x => x.Name == "Test" && x.Description == null).
+                    Select(x => x.Id).FirstOrDefault();
+
+            }
+            itemModel.Id = TestID;
+            itemModel.Name = "Test";
+            //Act
+            ViewResult view = controller.Edit(itemModel) as ViewResult;
+
+            TestID = view.ViewBag.Id;
+            //Assert
+            Assert.AreNotEqual(0, TestID);
+        }
+        [TestMethod]
+        public void Delete()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+            using (var context = new ShopBridgeDBEntities())
+            {
+                TestID = context.ItemMasters.Where(x=>x.Name=="Test" && x.Description==null).
+                    Select(x =>x.Id).FirstOrDefault();
+
+            }
+            //Act
+            ViewResult view = controller.Delete(TestID) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(view);
+        }
+    }
+}
